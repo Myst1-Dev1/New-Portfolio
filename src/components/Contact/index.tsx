918db359bca6f6ef06db3f55706851318fa11d1b
João Victor Vasconcelos Flôr <danteopzz@hotmail.com>
2023-10-10 00:29:17 -0300

    swiper and emailjs has been added

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -1,10 +1,28 @@
+import { useRef } from 'react';
 import { Button } from '../Button';
 import { SubTitle } from '../SubTitle';
 import styles from './styles.module.scss';
 
+import emailjs from '@emailjs/browser';
 import { FaPhone, FaEnvelope, FaMapMarkedAlt, FaPaperPlane } from 'react-icons/fa';
 
 export function Contact() {
+    const form:any = useRef();
+
+    function sendEmail(e:any) {
+        e.preventDefault();
+
+        emailjs.sendForm('service_88ls40o', 'template_qhba9q9', form.current, '7ywCWQ6iuPOTnxXC_')
+          .then((result) => {
+              alert('Mensagem enviada com sucesso !');
+              console.log(result);
+          }, (error) => {
+              alert('Ocorreu um erro ao enviar a mensagem !');
+              console.log('tivemos um erro', error)
+          });
+          e.target.reset();
+    }
+
     return (
         <div id='contact'>
             <SubTitle title="Contato" subtitle="Entre em contato" />
@@ -34,20 +52,20 @@ export function Contact() {
                             </div>
                         </div>
                     </div>
-                    <form className='col-md-6 d-flex flex-column gap-4'>
+                    <form ref={form} onSubmit={sendEmail} className='col-md-6 d-flex flex-column gap-4'>
                         <div className='row'>
                             <div className={`col-md-6 ${styles.inputBox}`}>
-                                <input type="text" placeholder='Nome' />
+                                <input type="text" placeholder='Nome' name='nome' />
                             </div>
                             <div className={`col-md-6 ${styles.inputBox}`}>
-                                <input type="email" placeholder='Email' />
+                                <input type="email" placeholder='Email' name='email' />
                             </div>
                         </div>
                         <div className={styles.inputBox}>
-                            <input type="tel" placeholder='Número' />
+                            <input type="tel" placeholder='Número' name='numero' />
                         </div>
                         <div className={styles.inputBox}>
-                            <textarea placeholder='Mensagem' />
+                            <textarea placeholder='Mensagem' name='mensagem' />
                         </div>
                         <Button type='submit'>
                             Enviar <FaPaperPlane className="text-light ms-5" />
--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -1,35 +1,68 @@
 import { SubTitle } from '../SubTitle';
 import styles from './styles.module.scss';
 
-import projectImage from '../../assets/images/mysflix.png';
+import mysFlix from '../../assets/images/mysflix.png';
+import msDashBoard from '../../assets/images/msdash.png';
+
 import { Button } from '../Button';
 import { FaArrowRight } from 'react-icons/fa';
 
+import { Navigation, Pagination, A11y } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+const projects = [
+    {
+        id: 1,
+        img: `${mysFlix}`,
+        title: 'MysFlix',
+        description: 'Clonagem da interface da Netflix, desenvolvida com React, Styled Components e a API do The Movie Database (TMDb).',
+        link: 'https://mysflix.netlify.app/'
+    },
+    {
+        id: 2,
+        img: `${msDashBoard}`,
+        title: 'MS Dashboard',
+        description: 'Projeto de dashboard desenvolvido com Next.js e Firebase. O dashboard possibilita a criação de contas de usuário, o login com as contas criadas e a criação de novos usuários.',
+        link: 'https://ms-dashboard.vercel.app/'
+    },
+]
+
 export function Projects() {
+
     return (
         <div id='projects'>
-            <SubTitle title="Portfolio"  subtitle="Projeto desenvolvidos"/>
-
+            <SubTitle title="Portfolio"  subtitle="Projeto em Destaque"/>
             <div className={`${styles.projects}`}>
-                <div className={`d-flex wrap justify-content-center gap-5 mt-5 ${styles.projectBox}`}>
-                    <div className={styles.imgContainer}>
-                        <img src={projectImage} alt="project-image" />
-                    </div>
-                    <div>
-                        <h3 className='fw-bold'>MysFlix</h3>
-                        <p className='mb-4'>Clonagem da interface da Netflix, <br />
-                            desenvolvida com React, Styled <br />
-                            Components e a API do The Movie <br />
-                            Database (TMDb).
-                        </p>
-                        <Button>
-                            <a href="/" target="_blank" rel="noreferrer">
-                                Demo <FaArrowRight className="ms-5" 
-                            />
-                            </a>
-                        </Button>
-                    </div>
-                </div>
+                <Swiper
+                    modules={[Navigation, Pagination, A11y]}
+                    slidesPerView={1}
+                    navigation
+                    pagination={{ clickable: true }}
+                >
+                    {projects.map(project => (
+                        <SwiperSlide key={project.id}>
+                            <div 
+                                className={`d-flex wrap justify-content-center gap-5 mt-5 mb-5 ${styles.projectBox}`}>
+                                <div className={styles.imgContainer}>
+                                    <img src={project.img} alt="project-image" />
+                                </div>
+                                <div className='d-flex flex-column justify-content-between'>
+                                    <h3 className='fw-bold'>{project.title}</h3>
+                                    <p className='mb-4'>{project.description}</p>
+                                    <Button>
+                                        <a href={project.link} target="_blank" rel="noreferrer">
+                                            Demo <FaArrowRight className="ms-5" 
+                                        />
+                                        </a>
+                                    </Button>
+                                </div>
+                            </div>
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
             </div>
         </div>
     )
