0fdb30402b9f45a73f466f6362baae6b3b1e30c3
João Victor <joao.victor@internit.net.br>
2024-06-17 11:47:34 -0300

    mudanças na formação

--- src/components/Formation/index.tsx
+++ src/components/Formation/index.tsx
@@ -1,21 +1,13 @@
 import styles from './styles.module.scss';
 
-import { FaUserGraduate, FaGraduationCap } from 'react-icons/fa';
+import { FaGraduationCap, FaCode } from 'react-icons/fa';
 
 export function Formation() {
     return (
         <div className={`${styles.formation}`}>
-            <h3 className='fw-bold'>Formação</h3>
+            <h3 className='fw-bold h4'>Formação/Experiência</h3>
             <div className={`d-flex flex-column gap-3 mt-5
                  ${styles.formationContainer}`}>
-                    <div className='d-flex gap-2'>
-                        <FaUserGraduate className={styles.icon} />
-                        <h6>Ensino médio completo</h6>
-                    </div>
-                    <div className='d-flex gap-2'>
-                        <FaUserGraduate className={styles.icon} />
-                        <h6>Auxiliar de operações em <br /> Logística - Senai Completo</h6>
-                    </div>
                     <div className='d-flex gap-2'>
                         <FaGraduationCap className={styles.icon} />
                         <h6>Análise e Desenvolvimento de <br /> sistemas - Unifatecie (cursando)</h6>
@@ -23,6 +15,10 @@ export function Formation() {
                     <div className='d-flex gap-2'>
                         <FaGraduationCap className={styles.icon} />
                         <h6>Curso de inglês - Kultivi (cursando)</h6>
+                    </div>
+                    <div className='d-flex gap-2'>
+                        <FaCode className={styles.icon} />
+                        <h6>Estágio - Front end (atualmente)</h6>
                     </div>
             </div>
         </div>
